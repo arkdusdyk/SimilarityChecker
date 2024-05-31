8e438396d9b00932926e605d667e5d7a104cbe65
yoyo <arkdusdyk@gmail.com>
2024-05-31 10:03:18 +0900

    Refactor - Unit Test, Feature/LengthCheck

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,28 +4,24 @@ using namespace std;
 
 TEST(SimilarityCheckerTest, lengthCheck) {
 	SimilarityChecker app;
-	vector<string> input = {"ASD", "DSA"};
-	double actual = app.lengthCheck(input);
+	double actual = app.lengthCheck("ASD", "DSA");
 	EXPECT_EQ(actual, 60);
 }
 
 TEST(SimilarityCheckerTest2, lengthCheck) {
 	SimilarityChecker app;
-	vector<string> input = { "A", "BB" };
-	double actual = app.lengthCheck(input);
+	double actual = app.lengthCheck("A", "BB");
 	EXPECT_EQ(actual, 0);
 }
 
 TEST(SimilarityCheckerTest3, lengthCheck) {
 	SimilarityChecker app;
-	vector<string> input = { "AAABB", "BAA" };
-	double actual = app.lengthCheck(input);
+	double actual = app.lengthCheck("AAABB", "BAA");
 	EXPECT_EQ(actual, 20);
 }
 
 TEST(SimilarityCheckerTest4, lengthCheck) {
 	SimilarityChecker app;
-	vector<string> input = { "AA", "AAE" };
-	double actual = app.lengthCheck(input);
+	double actual = app.lengthCheck("AA", "AAE");
 	EXPECT_EQ(actual, 30);
 }
--- SimilarityChecker/similarity_check.cpp
+++ SimilarityChecker/similarity_check.cpp
@@ -5,12 +5,11 @@ using namespace std;
 
 class SimilarityChecker {
 public:
-	int lengthCheck(vector<string> input) {
-		int longer_length = input[0].length();
-		int shorter_length = input[1].length();
+	int lengthCheck(string a, string b) {
+		int longer_length = a.length();
+		int shorter_length = b.length();
 		if (longer_length < shorter_length)
 			swap(longer_length, shorter_length);
-		//cout << longer_length << " " << shorter_length << "\n";
 		return (1 - (double)(longer_length - shorter_length) / shorter_length) * 60;
 	}
 };
