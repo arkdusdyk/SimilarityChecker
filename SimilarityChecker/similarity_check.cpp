85c739a49a46f19d284e25791ffed60004957795
yoyo <arkdusdyk@gmail.com>
2024-05-31 09:09:17 +0900

    Refactor - LengthCheck

--- SimilarityChecker/similarity_check.cpp
+++ SimilarityChecker/similarity_check.cpp
@@ -6,7 +6,7 @@ class SimilarityChecker {
 public:
 	double lengthCheck(vector<string> input) {
 		int longer_length = input[0].length();
-		int shorter_length = input[0].length();
+		int shorter_length = input[1].length();
 		if (longer_length < shorter_length)
 			swap(longer_length, shorter_length);
 		return 60 * (1 - (longer_length - shorter_length) / shorter_length);
